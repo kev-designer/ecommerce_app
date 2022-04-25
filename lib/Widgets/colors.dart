6706bc2ab381f67c72073fc96e738e526ff8d718
Keval Gadhiya <65587277+keval-gadhiya@users.noreply.github.com>
2022-04-25 16:58:45 +0530

    25th april

--- lib/Widgets/colors.dart
+++ lib/Widgets/colors.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ColorData {
-  static const primary = Color(0xFF006EF3);
+  static const primary = Color(0xFF9775FA);
+  static const secondary = Color(0xFFF5F6FA);
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF242424);
-  static const grey = Color(0xFF898989);
-  static const lightgrey = Color(0xFFE0E0E0);
+  static const grey = Color(0xFF8F959E);
   static const red = Colors.red;
+  static const green = Colors.green;
+  static const twitter = Color(0xFF1DA1F2);
+  static const fb = Color(0xFF4267B2);
+  static const google = Color(0xFFEA4335);
 }
